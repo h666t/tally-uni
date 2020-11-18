09035e378bef8970c88ad7ce1939297d47a6cbb1
h666t <1845720928@qq.com>
2020-11-18 20:16:24 +0800

    更新assets/icon‘

---
+++ pages/component/Statistics/StatisticsHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>
+    <Icon icon-name=""/>
+  </view>
+</template>
+
+<script>
+import Icon from "../public/Icon"
+export default {
+  components: {Icon}
+}
+</script>
+
+<style scoped lang="scss"></style>
